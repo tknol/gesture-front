17dc47ae10bbdd08dfd383109b8555297a00e410
Thomas <tom.n.knol@gmail.com>
2018-08-11 11:16:13 -0500

    adding basic nav and a register and signin component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 
 import Logo from './components/Logo/Logo';
+import Navigation from './components/Navigation/Navigation';
+import Signin from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 import './App.css';
 
 const app = new Clarifai.App({
@@ -22,13 +25,62 @@ const particlesOptions = {
 }
 
 class App extends Component {
+
+  constructor() {
+      super();
+      this.state = {
+        input: '',
+        imageUrl: '',
+        box: {},
+        route: 'signin',
+        isSignedIn: true,
+        user: {
+          id: '',
+          name: '',
+          email: '',
+          entries: 0,
+          joined: ''
+        }
+      }
+    }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+  loadUser = (data) => {
+  this.setState({user: {
+    id: data.id,
+    name: data.name,
+    email: data.email    
+  }})
+}
+
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
          <Particles className='particles'
           params={particlesOptions}
         />
-        <div><Logo /></div>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home'
+          ? <div>
+              <Logo />
+
+
+            </div>
+          : (
+             route === 'signin'
+             ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+             : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            )
+        }
       </div>
     );
   }
